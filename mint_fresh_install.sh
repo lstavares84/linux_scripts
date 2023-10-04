8978d9930ce7d4aca000d832f14857f4ab925a3f
Lucas S. Tavares <61010791+lstavares84@users.noreply.github.com>
2023-10-04 20:23:14 -0300

    Update mint_fresh_install.sh

--- mint_fresh_install.sh
+++ mint_fresh_install.sh
@@ -7,11 +7,14 @@ sudo apt install brave-browser -y
 
 
 #VS CODE
-sudo apt-get install wget gpg
+sudo apt-get install wget gpg -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg -y
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
+sudo apt install apt-transport-https -y
 sudo apt update
 sudo apt install code # or code-insiders
+
+#GPARTED
+sudo apt install gparted -y
