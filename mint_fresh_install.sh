02f0b9ff0dc2e9dd1beefd5c91f496b2a91b2a03
Lucas S. Tavares <61010791+lstavares84@users.noreply.github.com>
2023-10-08 15:03:18 -0300

    Update mint_fresh_install.sh

--- mint_fresh_install.sh
+++ mint_fresh_install.sh
@@ -16,7 +16,14 @@ sudo apt install apt-transport-https -y
 sudo apt update
 sudo apt install code # or code-insiders
 
+
+#### SMALL APPS
 #XMIND
 wget -O xmind.deb https://www.xmind.app/zen/download/linux_deb/
 sudo dpkg -i xmind.deb
 sudo rm xmind.deb
+#SPOTIFY
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
